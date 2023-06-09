2aa48f6e695817632d30e2252f05aa892324311a
marlon-morrison <ssmorrison0@gmail.com>
2023-06-09 11:09:20 -0500

    Added new basic arrays

--- DemoArray.java
+++ DemoArray.java
@@ -6,10 +6,26 @@ public class DemoArray {
       salaries[2] = 10.25;
       salaries[3] = 16.85;
       
+      double[] salaries2 = {6.25, 6.55, 10.25, 16.85};
+      
+      double[] salaries3 = {6.25, 6.55, 10.25, 16.85};
+      
       System.out.println("Salaries one by one are:");
       System.out.println(salaries[0]);
       System.out.println(salaries[1]);
       System.out.println(salaries[2]);
       System.out.println(salaries[3]);
+      System.out.println("");
+      
+      System.out.println("Salaries 2v one by one are:");
+      System.out.println(salaries2[0]);
+      System.out.println(salaries2[1]);
+      System.out.println(salaries2[2]);
+      System.out.println(salaries2[3]);
+      System.out.println("");
+      
+      System.out.println("Salaries 3v one by one are:");
+      for(int x = 0; x < salaries3.length; ++x)
+         System.out.println(salaries3[x]);
    }
 }
